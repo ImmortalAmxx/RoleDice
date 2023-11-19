18aefb7f4428d8e7b5b5d4ed3bd82a7329a9d802
mmortalmxx <bankaitensazangetsu1337@gmail.com>
2023-11-19 14:36:10 +0200

    Step 3 - Display one of the picture.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -17,6 +17,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -43,7 +44,7 @@ class MainActivity : AppCompatActivity() {
     /**
      * Roll the dice and update the screen with the result.
      */
-    private fun rollDice() {
+    /*private fun rollDice() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
@@ -51,6 +52,14 @@ class MainActivity : AppCompatActivity() {
         // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+    }*/
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_2)
     }
 }
 
